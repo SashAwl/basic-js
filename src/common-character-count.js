265c61ac3606213ab6d34de03e9d69cd654c4158
Alexandra Zaporenko <aleks.shilova@gmail.com>
2025-01-06 13:03:19 +0300

    feat: add solutions for carbon-dating, common-char-count, count-cats, delete-digit tasks

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  let count = 0;
+  const uniqCharS1 = new Set(s1);
+
+  for (const char of uniqCharS1) {
+ 
+    while (s1.includes(char) && s2.includes(char)) {
+      count +=1;
+      s1 = s1.replace(char, "");
+      s2 = s2.replace(char, "");
+    }
+  }
+
+  return count;
 }
 
 module.exports = {
--- src/delete-digit.js
+++ src/delete-digit.js
@@ -1,8 +1,8 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
 /**
- * Given some integer, find the maximal number you can obtain
- * by deleting exactly one digit of the given number.
+   * Given some integer, find the maximal number you can obtain
+   * by deleting exactly one digit of the given number.
  *
  * @param {Number} n
  * @return {Number}
@@ -11,9 +11,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  maxN = 0;
+  strN = n.toString();
+  for (const char of strN) {
+    newN = +strN.replace(char, "");
+    maxN = Math.max(maxN, newN);
+  }
+
+  return maxN;
 }
 
 module.exports = {
